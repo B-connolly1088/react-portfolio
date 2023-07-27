081d908832de4758f8faeff8c5d9c63aa5803e6f
Ben laptop <b.connolly1088@gmail.com>
2023-07-26 19:24:00 -0600

    worked in resume.js portfolio.js and cardTemplate.js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import CardTemplate from '../CardTemplate';
-import hotTakes from '../images/Hot-Takes.png';
-import drDrink from "../images/Dr-Drink.png";
+import React from "react";
+import CardTemplate from "../CardTemplate";
+import hotTakes from "../../images/Hot-Takes.png";
+import drDrink from "../../images/Dr-Drink.png";
 
 export default function Portfolio() {
   const projects = [
@@ -10,52 +10,51 @@ export default function Portfolio() {
       description: "Fluff",
       github: "https://github.com/B-connolly1088/Hot-Takes",
       deployed: "https://hot-takes-reviews-b29812e3e846.herokuapp.com/",
-      src: hotTakes
+      src: hotTakes,
     },
     {
       title: "Dr. Drink",
       description: "Fluff",
       github: "https://github.com/B-connolly1088/Dr-Drink",
       deployed: "https://hot-takes-reviews-b29812e3e846.herokuapp.com/",
-      src: drDrink
+      src: drDrink,
     },
     {
       title: "Employee Tracker",
       description: "Fluff",
       github: "https://github.com/B-connolly1088/employee-tracker",
-      src: drDrink
+      src: drDrink,
     },
     {
       title: "ECommerce Backend",
       description: "Fluff",
       github: "https://github.com/B-connolly1088/ecommerce-backend",
-      src: drDrink
+      src: drDrink,
     },
     {
       title: "SocialAPI",
       description: "Fluff",
       github: "https://github.com/B-connolly1088/social-api",
-      src: drDrink
+      src: drDrink,
     },
     {
       title: "Dr. Drink",
       description: "Fluff",
       github: "https://github.com/B-connolly1088/Dr-Drink",
       deployed: "https://hot-takes-reviews-b29812e3e846.herokuapp.com/",
-      src: drDrink
+      src: drDrink,
     },
-
-  ]
+  ];
   return (
     <div>
       <h1>Portfolio</h1>
-    <div className='row'>
-    {projects.map((project, i) => (
-      <div key={i} className='col-md-4 col-sm-12'>
-         <CardTemplate project = {project} />
-        </div>
-    ))}
-    </div>
+      <div className="row">
+        {projects.map((project, i) => (
+          <div key={i} className="col-md-4 col-sm-12">
+            <CardTemplate project={project} portfolio={true} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import CardTemplate from '../CardTemplate';
+import myResume from '../../images/resume.pdf'
+
 
 export default function Resume() {
+  const resume = {
+    src: "#",
+    deployed: myResume
+  }
   return (
     <div>
       <h1>Resume</h1>
-      
+      <CardTemplate project = {resume} />
     </div>
   );
 }
