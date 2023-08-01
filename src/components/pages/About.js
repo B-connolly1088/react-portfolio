ea5585d95f9f232830df003c9f0492a461a8d403
Ben laptop <b.connolly1088@gmail.com>
2023-08-01 16:07:04 -0600

    worked in portfolio and about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import Headshot from "../../images/Headshot.jpg"
 
 export default function About() {
   return (
     <div className='page-content'>
-      <h1>Bio</h1>
+      <h1>About The Developer</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        My name is Benjamin Connolly and am originally from the Southeastern United States. I strive to bring Southern Hospitality into the development world.
       </p>
+      <h2>Languages</h2>
+      <ul>
+        <li>JS</li>
+        <li>HTML</li>
+        <li>CSS</li>
+      </ul>
     </div>
   );
 }
